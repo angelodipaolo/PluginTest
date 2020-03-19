5288b9b1bd0858cf4232fc020f4d910bd6d740db
Angelo Di Paolo <angelojdipaolo@icloud.com>
2020-03-19 11:28:18 -0400

    add registry plugin

---
+++ Plugins/Registry.framework/Headers/Registry.h
@@ -0,0 +1,19 @@
+//
+//  Registry.h
+//  Registry
+//
+//  Created by Angelo Di Paolo on 3/19/20.
+//  Copyright © 2020 Angelo Di Paolo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Registry.
+FOUNDATION_EXPORT double RegistryVersionNumber;
+
+//! Project version string for Registry.
+FOUNDATION_EXPORT const unsigned char RegistryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Registry/PublicHeader.h>
+
+
